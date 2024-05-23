e04a1d77478a322d148eb24e1f24dc46fb6900fe
Makutu369 <brightdreamfuture6@gmail.com>
2024-05-23 03:39:41 +0000

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Hello customer</div>;
+};
+
+export default page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>invoice page</div>;
+};
+
+export default page;
